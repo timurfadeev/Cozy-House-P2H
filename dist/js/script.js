0b4e2550afd640a852db77bfaf3bd6f4dddb3cac
Timur <61089284+timurfadeev@users.noreply.github.com>
2021-03-12 00:06:46 +0200

    added slider to cards

---
+++ dist/js/script.js
@@ -0,0 +1,12 @@
+$(".pets__row").slick({
+  arrows: true,
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: "<img src='./img/pets/icons/left.jpg' class='prev' alt='1'>",
+  nextArrow: "<img src='./img/pets/icons/right.jpg' class='next' alt='2'>",
+  // centerMode: true,
+  // variableWidth: true,
+});
