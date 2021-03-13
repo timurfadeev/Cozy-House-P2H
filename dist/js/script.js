d37e8516c89a0d2c224dd3da3bacff4b915a19a1
Timur <61089284+timurfadeev@users.noreply.github.com>
2021-03-13 17:11:24 +0200

    added burger, changed the structure to adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,57 @@
-$(".pets__row").slick({
-  arrows: true,
-  infinite: true,
-  // autoplay: true,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  prevArrow: "<img src='./img/pets/icons/left.jpg' class='prev' alt='1'>",
-  nextArrow: "<img src='./img/pets/icons/right.jpg' class='next' alt='2'>",
-  // centerMode: true,
-  // variableWidth: true,
+$(document).ready(function () {
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    let id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 700);
+  });
+});
+
+$(document).ready(function () {
+  let e = 0;
+
+  function n(e) {
+    e > 0
+      ? $(".header").addClass("active")
+      : $(".header").removeClass("active");
+  }
+
+  n(window.pageYOffset),
+    $(".burger").on("click", function () {
+      $(".burger,.header__nav-burger").toggleClass("active"),
+        $("body").toggleClass("lock");
+    }),
+    function a(e) {
+      $(".burger,.header__nav-burger").removeClass("active"),
+        $("body").removeClass("lock");
+      let n = Math.ceil($("." + e).offset().top) - 50;
+      $("html, body").animate(
+        {
+          scrollTop: n,
+        },
+        500
+      );
+    };
+
+  t(window.innerWidth),
+    window.addEventListener("resize", function () {
+      let e = window.innerWidth;
+      t(e),
+        e > 700 ? $("body").removeClass("lock") : $("body").addClass("lock");
+    }),
+    $(".header__logo").on("click", function () {
+      a("page");
+    }),
+    $(".nav-link-about").on("click", function () {
+      a("about");
+    }),
+    $(".nav-link-pets").on("click", function () {
+      a("pets");
+    }),
+    $(".nav-link-helper").on("click", function () {
+      a("help");
+    }),
+    $(".nav-link-contacts").on("click", function () {
+      a("footer");
+    });
 });
